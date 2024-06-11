b5e146072e7f46a7f110b5c0cb1b89f7707a27b0
Ho-Da-Dak <godqhr721@gmail.com>
2024-06-11 16:04:40 +0900

    init : sequelize init

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const dotenv = require('dotenv');
+const path = require('path');
 const express = require('express');
 const http = require('http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger/swagger-output');
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+const db = require('./models');
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('DB 연결 완료');
+});
 
 const app = express();
 const server = http.createServer(app);
