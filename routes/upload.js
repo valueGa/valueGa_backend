f419d7b9d5597901d8768e244a16f29e8f74322f
ChaeyoonWoo <heeni1004@gmail.com>
2024-06-15 16:12:32 +0900

    feat: db에 엑셀=>바이너리 저장
    #11

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const { USER_TEMPLATE_TEMPORARY } = require('../models');
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const { USER_TEMPLATES } = require("../models");
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, './uploads'));
+    cb(null, path.join(__dirname, "./uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, `a.xlsx`);
@@ -16,40 +16,31 @@ const storage = multer.diskStorage({
 });
 
 const excelFilePath = path.join(__dirname, `./uploads/a.xlsx`);
-const outputFilePath = path.join(__dirname, './uploads/abx_binary.txt');
+const outputFilePath = path.join(__dirname, "./uploads/abx_binary.txt");
 const upload = multer({ storage: storage });
 
-router.post('/', upload.single('file'), async (req, res) => {
+router.post("/", upload.single("file"), async (req, res) => {
   const file = req.file;
 
   if (!file) {
-    return res.status(400).send('No file uploaded');
+    return res.status(400).send("No file uploaded");
   }
 
   try {
-    const fileContent = fs.readFileSync(excelFilePath, 'utf-8');
+    const fileContent = fs.readFileSync(excelFilePath);
 
-    const binaryContent = Buffer.from(fileContent, 'utf-8').toString('binary');
-  } catch (error) {
-    console.error('Error processing file:', error);
-  }
+    const binaryContent = Buffer.from(fileContent).toString("binary");
 
-  try {
-    await USER_TEMPLATE_TEMPORARY.create({
+    await USER_TEMPLATES.create({
       user_id: 1,
       stock_id: 1,
-      template_name: file.originalname,
       created_date: new Date(),
-      updated_date: new Date(),
       excel_data: binaryContent,
     });
-
-    res
-      .status(200)
-      .send('File uploaded, converted, and saved to database and disk');
+    console.log("파일업로드완");
+    res.status(200).send("파일 업로드완");
   } catch (error) {
-    console.error('Error processing file:', error);
-    res.status(500).send('Error processing file');
+    console.error("Error processing file:", error);
   }
 });
 
