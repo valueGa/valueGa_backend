edd037eb56c00dc63bef376fb22903c3b11c1d16
Ho-Da-Dak <godqhr721@gmail.com>
2024-06-18 13:09:56 +0900

    feature : finance_info 모델 수정
    #27

--- models/finance_infos.js
+++ models/finance_infos.js
@@ -1,88 +1,82 @@
-// module.exports = (sequelize, Sequelize) => {
-//   const FINANCE_INFO = sequelize.define(
-//     'FINANCE_INFOS',
-//     {
-//       stock_id: {
-//         type: Sequelize.INTEGER,
-//         references: {
-//           model: 'STOCKS',
-//           key: 'stock_id',
-//         },
-//       },
-//       year: {
-//         type: Sequelize.DATE,
-//       },
-//       sr: {
-//         type: Sequelize.STRING(10),
-//       },
-//       npcf: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       gpf: {
-//         type: Sequelize.STRING(10),
-//       },
-//       sense: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       se: {
-//         type: Sequelize.STRING(10),
-//       },
-//       ar: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       bais: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       oi: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       capex: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       dnp: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       as: {
-//         type: Sequelize.STRING(10),
-//       },
-//       re: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       lbt: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       rd: {
-//         type: Sequelize.DECIMAL,
-//       },
-//       roe: {
-//         type: Sequelize.DOUBLE,
-//       },
-//       eps: {
-//         type: Sequelize.DOUBLE,
-//       },
-//       bps: {
-//         type: Sequelize.DOUBLE,
-//       },
-//       evebitda: {
-//         type: Sequelize.STRING(10),
-//       },
-//       dr: {
-//         type: Sequelize.STRING(10),
-//       },
-//       rr: {
-//         type: Sequelize.STRING(10),
-//       },
-//     },
-//     {
-//       freezeTableName: true,
-//       indexes: [
-//         {
-//           unique: true,
-//           fields: ['stock_id', 'year'],
-//         },
-//       ],
-//     }
-//   );
+module.exports = (sequelize, Sequelize) => {
+  const FINANCE_INFO = sequelize.define(
+    'FINANCE_INFOS',
+    {
+      stock_id: {
+        type: Sequelize.STRING(6),
+        references: {
+          model: 'STOCKS',
+          key: 'stock_id',
+        },
+      },
+      year: {
+        type: Sequelize.INTEGER,
+      },
+      ep: {
+        type: Sequelize.DOUBLE,
+      },
+      el: {
+        type: Sequelize.DOUBLE,
+      },
+      se: {
+        type: Sequelize.DOUBLE,
+      },
+      fl: {
+        type: Sequelize.DOUBLE,
+      },
+      fp: {
+        type: Sequelize.DOUBLE,
+      },
+      ite: {
+        type: Sequelize.DOUBLE,
+      },
+      sr: {
+        type: Sequelize.DOUBLE,
+      },
+      oi: {
+        type: Sequelize.DOUBLE,
+      },
+      ibt: {
+        type: Sequelize.DOUBLE,
+      },
+      fpl: {
+        type: Sequelize.DOUBLE,
+      },
+      epl: {
+        type: Sequelize.DOUBLE,
+      },
+      rr: {
+        type: Sequelize.FLOAT,
+      },
+      dr: {
+        type: Sequelize.FLOAT,
+      },
+      evebitda: {
+        type: Sequelize.FLOAT,
+      },
+      roe: {
+        type: Sequelize.FLOAT,
+      },
+      bps: {
+        type: Sequelize.FLOAT,
+      },
+      ni: {
+        type: Sequelize.FLOAT,
+      },
+      ts: {
+        type: Sequelize.DOUBLE,
+      },
+    },
+    {
+      freezeTableName: true,
+      indexes: [
+        {
+          unique: true,
+          fields: ['stock_id', 'year'],
+        },
+      ],
+    }
+  );
 
-//   return FINANCE_INFO;
-// };
+  return FINANCE_INFO;
+};
