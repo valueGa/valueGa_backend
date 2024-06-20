8c5bffde86915c8a941aad0a45df4efaf18ab998
Ho-Da-Dak <godqhr721@gmail.com>
2024-06-20 19:16:45 +0900

    feat: accessToken 만료시 재발급 API 구현
    #50

--- service/getAccessToken.js
+++ service/getAccessToken.js
@@ -1,25 +1,41 @@
 const axios = require('axios');
-const fs = require('fs');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config({ path: '../.env' });
 
 const BASE_URL = 'https://openapi.koreainvestment.com:9443';
 const URL = '/oauth2/tokenP';
 
-axios({
-    method: 'post',
-    url: BASE_URL + URL,
-    data: {
+async function getAccessToken() {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: BASE_URL + URL,
+      data: {
         grant_type: 'client_credentials',
         appkey: process.env.HANTO_APPKEY,
         appsecret: process.env.HANTO_SECRETKEY,
-    },
-    headers: {
+      },
+      headers: {
         'Content-Type': 'application/json',
-    },
-    params: {},
-    responseType: 'json',
-}).then((response) => {
-    console.log(response.data);
-});
+      },
+      params: {},
+      responseType: 'json',
+    });
+
+    const token = response.data.access_token;
+    const bearer = 'Bearer ';
+    const accessToken = bearer + token;
+
+    fs.writeFileSync('token.dat', accessToken);
+
+    return accessToken;
+  } catch (error) {
+    console.error('Access token 갱신 중 오류 발생:', error);
+    throw error;
+  }
+}
+
+exports.getAccessToken = getAccessToken;
